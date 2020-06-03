ab27463ba753db93af31072fbaa558b5fdc76ab3
Timothy Miller <tim@themillerfamily.co.nz>
2020-06-03 15:15:45 +1200

    removed uneeded code

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,29 +1,3 @@
-// import React, { Component } from 'react';
-// import { Map, GoogleApiWrapper } from 'google-maps-react';
-
-// const mapStyles = {
-//     width: '100%',
-//     height: '80%'
-// };
-
-
-// export class MapContainer extends Component {
-//     render() {
-//         return (
-//             <Map
-//                 google={this.props.google}
-//                 zoom={8}
-//                 style={mapStyles}
-//                 initialCenter={{ lat: 47.444, lng: -122.176 }}
-//             />
-//         );
-//     }
-// }
-
-// export default GoogleApiWrapper({
-//     apiKey: 'AIzaSyAAl2Dh5aawLCccxyHORTsi7uSq6vVrcDE'
-//   })(MapContainer);
-
 import React from "react";
 import { GoogleMap, withScriptjs, withGoogleMap } from "google-maps-react";
 
