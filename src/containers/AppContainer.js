fa5cfbf1ebec6173b179140a17840e154f74b48a
Timothy Miller <tim@themillerfamily.co.nz>
2020-06-04 21:38:17 +1200

    added cos to test for v3

--- src/containers/AppContainer.js
+++ src/containers/AppContainer.js
@@ -6,6 +6,97 @@ import Map from '../components/Map';
 import ZipResponse from '../components/ZipResponse';
 import Zip from '../components/Zip';
 
+// import * as ibmdb from 'ibm_db';
+
+// const ibmdb = require('ibm_db');
+
+// const connStr = `DSN=dashdb-txn-sbox-yp-dal09-04.services.dal.bluemix.net;UID=tzh98377;PWD=4f0l-r0qtrj8l8s5`
+
+// ibmdb.open(connStr, function(err, conn) {
+//      //...
+// });
+
+
+var ibm = require('ibm-cos-sdk');
+var util = require('util');
+
+var config = {
+    endpoint: 'https://control.cloud-object-storage.cloud.ibm.com/v2/endpoints',
+    apiKeyId: 'Vi2B5HNZfUq7b5sNwVKxZQcubaD2FodxT41U3BmjLi8B',    
+    serviceInstanceId: 'crn:v1:bluemix:public:cloud-object-storage:global:a/af144da82cb544af93c55c819c4304be:ffa161a1-108e-4569-9c6d-d4692f2258ff::',
+};
+
+// {
+//     "apikey": "Vi2B5HNZfUq7b5sNwVKxZQcubaD2FodxT41U3BmjLi8B",
+//     "endpoints": "https://control.cloud-object-storage.cloud.ibm.com/v2/endpoints",
+//     "iam_apikey_description": "Auto-generated for key f709ef42-baa2-4943-b2f2-0be560bd59d4",
+//     "iam_apikey_name": "Service credentials-1",
+//     "iam_role_crn": "crn:v1:bluemix:public:iam::::serviceRole:Writer",
+//     "iam_serviceid_crn": "crn:v1:bluemix:public:iam-identity::a/af144da82cb544af93c55c819c4304be::serviceid:ServiceId-7a96d934-ab4c-4c7b-8a39-1caf10f5aca0",
+//     "resource_instance_id": "crn:v1:bluemix:public:cloud-object-storage:global:a/af144da82cb544af93c55c819c4304be:ffa161a1-108e-4569-9c6d-d4692f2258ff::"
+//   }
+
+var cos = new ibm.S3(config);
+
+function doCreateBucket() {
+    console.log('Creating bucket');
+    return cos.createBucket({
+        Bucket: 'my-bucket',
+        CreateBucketConfiguration: {
+          LocationConstraint: 'us-standard'
+        },
+    }).promise();
+}
+
+function doCreateObject() {
+    console.log('Creating object');
+    return cos.putObject({
+        Bucket: 'my-bucket',
+        Key: 'foo',
+        Body: 'bar'
+    }).promise();
+}
+
+function doDeleteObject() {
+    console.log('Deleting object');
+    return cos.deleteObject({
+        Bucket: 'my-bucket',
+        Key: 'foo'
+    }).promise();
+}
+
+function doDeleteBucket() {
+    console.log('Deleting bucket');
+    return cos.deleteBucket({
+        Bucket: 'my-bucket'
+    }).promise();
+}
+
+doCreateBucket()
+    .then(doCreateObject)
+    .then(doDeleteObject)
+    .then(doDeleteBucket)
+    .then(function() {
+        console.log('Finished!');
+    })
+    .catch(function(err) {
+        console.error('An error occurred:');
+        console.error(util.inspect(err));
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 function handleApiResult(json) {
     if (json.cod === '400' || json.cod === '404') {
         return {
@@ -34,18 +125,20 @@ function AppContainer(props) {
     const handleZipChange = async (input) => {
         var numbers = /^[0-9]+$/;
         if (input.match(numbers)) {
-            callApiEndpoint(`https://api.openweathermap.org/data/2.5/weather?appid=6b7b471967dd0851d0010cdecf28f829&units=metric&zip=${input},us`);
+            callApi(`https://api.openweathermap.org/data/2.5/weather?appid=6b7b471967dd0851d0010cdecf28f829&units=metric&zip=${input},us`);
         } else {
-            callApiEndpoint(`https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=6b7b471967dd0851d0010cdecf28f829&units=metric`);
+            callApi(`https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=6b7b471967dd0851d0010cdecf28f829&units=metric`);
         }
     }
 
-    const callApiEndpoint = async (url, panTo) => {
+    // Handles all the interaction with the weather api
+    const callApi = async (url, panTo) => {
         const res = await fetch(url);
         const json = await res.json();
         const responseData = handleApiResult(json);
         setResponseData(responseData);
 
+        // Make the map focus on the targeted location
         if (responseData.data !== null) {
             mapRef.current.panTo({
                 lat: responseData.data.coord.lat,
@@ -79,12 +172,12 @@ function AppContainer(props) {
                 justifyContent: 'center',
             }}>
                 <div style={{
-                    height: '50vh',
-                    width: '50vw',
+                    height: '80vh',
+                    width: '80vw',
                 }}>
                     <Map
                         innerRef={mapRef}
-                        googleMapURL={`https://www.google.com/maps/api/js?v3/.exp&libraries=geometry,drawing,places&key=AIzaSyAAl2Dh5aawLCccxyHORTsi7uSq6vVrcDE`}
+                        googleMapURL={`https://www.google.com/maps/api/js?v3/.exp&libraries=geometry,drawing,places&key=AIzaSyCDENbbAdPMe0m-BnR9qatcVlX0mshY1j8`}
                         loadingElement={<div style={{ height: "100%" }} />}
                         containerElement={<div style={{ height: "100%" }} />}
                         mapElement={<div style={{ height: "100%" }} />}
@@ -92,8 +185,8 @@ function AppContainer(props) {
                             const lat = event.latLng.lat();
                             const lng = event.latLng.lng();
 
-                            // Call api endpoint for lat/lng
-                            callApiEndpoint(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=6b7b471967dd0851d0010cdecf28f829&units=metric`);
+                            // Call the api for the clicked on latitude/longitude
+                            callApi(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=6b7b471967dd0851d0010cdecf28f829&units=metric`);
                         }}
                     >
                         {responseData.data !== null && (
